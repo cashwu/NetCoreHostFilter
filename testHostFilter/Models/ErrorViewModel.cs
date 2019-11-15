ff8ad77ad9d62e3797f1ec3f0725a5d5360d9fb2
cash <cash@cashwu.com>
2019-11-15 09:52:19 +0800

    :tada: Initial commit.

---
+++ testHostFilter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testHostFilter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
